bcf6e2734bb7e02c389b516268f75b9d06d8c490
Jason Garcia <146444354+RustyDL@users.noreply.github.com>
2023-11-02 21:05:29 -0400

    removed form in html and added a promp for users.

--- javascript.js
+++ javascript.js
@@ -1,22 +1,17 @@
 function countFroyoFlavors(input) {
-  const flavors = input.split(",");
-  const flavorCount = {};
+    const flavors = input.split(',');
+    const flavorCount = {};
 
-  for (let i = 0; i < flavors.length; i++) {
-    const flavor = flavors[i].trim();
-    flavorCount[flavor] = (flavorCount[flavor] || 0) + 1;
-  }
+    for (let i = 0; i < flavors.length; i++) {
+        const flavor = flavors[i].trim();
+        flavorCount[flavor] = (flavorCount[flavor] || 0) + 1;
+    }
 
-  return flavorCount;
+    return flavorCount;
 }
 
-function displayFlavorCountsTable(flavorCounts) {
-  console.table(flavorCounts);
-}
+const userInput = prompt("Enter a list of froyo flavors:", "Separated each with a comma" );
+const flavorCounts = countFroyoFlavors(userInput);
 
-document.getElementById("flavorForm").addEventListener("submit", function (e) {
-  e.preventDefault();
-  const userInput = document.getElementById("flavorInput").value;
-  const flavorCounts = countFroyoFlavors(userInput);
-  displayFlavorCountsTable(flavorCounts);
-});
+console.log("Froyo Flavor Counts:");
+console.table(flavorCounts);
